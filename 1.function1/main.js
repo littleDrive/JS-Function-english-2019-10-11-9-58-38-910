c84f00647288ce45cc150f8472b55e7ee9943202
小司机 <36127072+littleDrive@users.noreply.github.com>
2020-06-24 23:35:32 +0800

    Create main.js

---
+++ 1.function1/main.js
@@ -0,0 +1,11 @@
+function reverseString(message){
+    // wirte your code here
+    if (message.length <= 0) {
+        return "";
+    }
+    let s = [];
+    for (let i = message.length - 1; i >= 0; i --) {
+        s.push(message[i]);
+    }
+    return s.join("");
+  }
